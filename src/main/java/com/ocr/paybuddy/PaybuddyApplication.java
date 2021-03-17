1dcf6f76a253ca89b5db1e470b1a0dcd71e156e9
Jason <jasondubourg@gmail.com>
2021-03-17 21:18:17 +0100

    Initial commit

---
+++ src/main/java/com/ocr/paybuddy/PaybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.paybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaybuddyApplication.class, args);
+	}
+
+}
